300191d16193b73b69cd0325ac89b141c5fce1d8
Mateusz Bernaciak <programistamati@gmail.com>
2021-10-04 19:54:19 +0200

    push

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import { accordion } from "./accordion";
-import { effect } from "./effect";
-import { nav } from "./nav";
-import { scrollup } from "./scrollup";
-import { search } from "./search";
-import "./css/home.css";
